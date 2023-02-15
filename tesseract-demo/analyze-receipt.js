bf066ad59bb3c325cbfb679106b0b795d67f6164
Jacob <jacobfisher311@gmail.com>
2023-02-15 15:26:49 -0800

    small changes

--- tesseract-demo/analyze-receipt.js
+++ tesseract-demo/analyze-receipt.js
@@ -4,6 +4,14 @@ const worker = await createWorker();
 async function analyze_receipt(input) {
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
+    /**
+     * Creation of Line data under the variable "lines" 
+     * Line data consists of a collection of words, and
+     * corresponding confidence. Currently this function
+     * creates a 2D array "arr" that contains line data,
+     * and the corresponding confidence for the entire 
+     * line. 
+    **/
     const { data: { lines } } = await worker.recognize(input);
     
     let arr = new Array(lines.length);
--- tesseract-demo/index.js
+++ tesseract-demo/index.js
@@ -1,13 +1,12 @@
 /**********************************************************
  * To run, type in terminal: 
  *                           npm i
- *                           node index.js [imagepath]
+ *                           node index.js
  *********************************************************/
 import analyze_receipt from './analyze-receipt.js';
 import preprocess from './image-preprocessing.js';
 
 let defaultImagePath = "receipts/receipt2.jpeg";
-
 
 let imagePath = await preprocess(defaultImagePath);
 let arr = await analyze_receipt(imagePath);
